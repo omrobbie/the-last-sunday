2f5dd7ed41492062aab33dd92f9003f7191d41de
omrobbie <putu.roby@yahoo.com>
2023-03-23 03:41:47 +0700

    remove footer, todo, and done list

--- main.js
+++ main.js
@@ -5,24 +5,37 @@ var ageExpectancy=80;
 var totalWeeksInLife=ageExpectancy*52.1429;
 var birthdayDate;
 
-var stuffDone;
-var stuffToDo;
-
 $('document').ready(function() {
-    //set onclick handlers
     $('#onclick_setDate').click(setDate);
-    $('#stuffDoneButton').click(saveStuffDone);
-    $('#stuffTodoButton').click(saveStuffTodo);
-    $('#onclick_showInspiration').click(showInspiration);
-    $('#onclick_showEdits').click(showEdits);
-
     calculateEverything();
-
-    if(localStorage.bdayInput) {}
-
-    updateStuffDone();
-    updateStuffTodo();
 });
+
+function setDate() {
+    localStorage.bdayInput = $('#bdayInput').val();
+    localStorage.nameInput = $('#nameInput').val();
+    $('#ageInput').hide();
+    calculateEverything();
+}
+
+function calculateEverything() {
+    if(localStorage.bdayInput) {
+        weeksLeft=returnWeeks(localStorage.bdayInput);
+
+        if(localStorage.nameInput) prependString=localStorage.nameInput+", only ";
+        else prependString="Only "
+
+        $('#weeksLeft').html(prependString+weeksLeft+" Sundays remain");
+        $('#ageInput').hide();
+
+        plotLifeChart();
+    } else {
+        $('#weeksLeft').html("Please tell us your name and when you were born");
+        $('#ageInput').show();
+    }
+
+    $('#bdayInput').val(localStorage.bdayInput);
+    $('#nameInput').val(localStorage.nameInput);
+}
 
 //birthday is of type YYYYMMDD like 19870603
 function returnWeeks(birthday) {
@@ -32,21 +45,6 @@ function returnWeeks(birthday) {
     yearOfBirth=bday.getFullYear();
     birthdayDate=bday;
     return Math.ceil(totalWeeksInLife - ageDifMs/(1000*60*60*24*7));
-}
-
-function getWeekNumber(d) {
-    // Copy date so don't modify original
-    d = new Date(+d);
-    d.setHours(0,0,0);
-    // Set to nearest Thursday: current date + 4 - current day number
-    // Make Sunday's day number 7
-    d.setDate(d.getDate() + 4 - (d.getDay()||7));
-    // Get first day of year
-    var yearStart = new Date(d.getFullYear(),0,1);
-    // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
-    // Return array of year and week number
-    return [d.getFullYear(), weekNo];
 }
 
 function plotLifeChart() {
@@ -100,86 +98,6 @@ function plotLifeChart() {
     }
 }
 
-function updateStuffDone() {
-    if(localStorage.stuffDone) {
-        $('#listStuffDone').html("");
-        fillstuff($('#listStuffDone'),localStorage.stuffDone);
-        $('#stuffDone').val(localStorage.stuffDone);
-        $('#stuffDoneContainer').hide();
-        $('#listStuffDone').show();
-    } else {
-        $('#stuffDoneContainer').show();
-    }
-}
-
-function updateStuffTodo() {
-    if(localStorage.stuffTodo) {
-        $('#listStuffTodo').html("");
-        fillstuff($('#listStuffTodo'),localStorage.stuffTodo);
-        $('#stuffTodo').val(localStorage.stuffTodo);
-        $('#stuffTodoContainer').hide();
-        $('#listStuffTodo').show();
-    } else {
-        $('#stuffTodoContainer').show();
-    }
-}
-
-function fillstuff(elem,stuff) {
-    //break into newlines
-    var lines = stuff.split(/\n/);
-    var texts = [];
-
-    for (var i=0; i < lines.length; i++) {
-        // only push this line if it contains a non whitespace character.
-        if (/\S/.test(lines[i])) {
-            elem.append("<li>"+$.trim(lines[i])+"</li>");
-        }
-    }
-}
-
-function calculateEverything() {
-    if(localStorage.bdayInput) {
-        weeksLeft=returnWeeks(localStorage.bdayInput);
-
-        if(localStorage.nameInput) prependString=localStorage.nameInput+", only ";
-        else prependString="Only "
-
-        $('#weeksLeft').html(prependString+weeksLeft+" Sundays remain");
-        $('#title').html('How are you going to spend these weeks'+(localStorage.nameInput ? " "+localStorage.nameInput : "")+"?");
-        $('#ageInput').hide();
-
-        plotLifeChart();
-    } else {
-        $('#weeksLeft').html("Please tell us your name and when you were born");
-        $('#ageInput').show();
-    }
-
-    $('#bdayInput').val(localStorage.bdayInput);
-    $('#nameInput').val(localStorage.nameInput);
-}
-
-function setDate() {
-    localStorage.bdayInput = $('#bdayInput').val();
-    localStorage.nameInput = $('#nameInput').val();
-    $('#ageInput').hide();
-    calculateEverything();
-}
-
-function getWeekNumber(d) {
-    // Copy date so don't modify original
-    d = new Date(+d);
-    d.setHours(0,0,0);
-    // Set to nearest Thursday: current date + 4 - current day number
-    // Make Sunday's day number 7
-    d.setDate(d.getDate() + 4 - (d.getDay()||7));
-    // Get first day of year
-    var yearStart = new Date(d.getFullYear(),0,1);
-    // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7)
-    // Return array of year and week number
-    return [d.getFullYear(), weekNo];
-}
-
 function weeksInYear(year) {
     var month = 11, day = 31, week;
 
@@ -208,27 +126,17 @@ function days_between(date1, date2) {
     return Math.round(difference_ms/ONE_DAY)
 }
 
-function showInspiration() {
-    $('#leftpane').show();
-    $('#rightpane').show();
-}
-
-
-function showEdits() {
-    showInspiration();
-    $('#ageInput').show();
-    $('#stuffTodoContainer').show();
-    $('#stuffDoneContainer').show();
-    $('#listStuffTodo').hide();
-    $('#listStuffDone').hide();
-}
-
-function saveStuffDone() {
-    localStorage.stuffDone = $('#stuffDone').val();
-    updateStuffDone();
-}
-
-function saveStuffTodo() {
-    localStorage.stuffTodo = $('#stuffTodo').val();
-    updateStuffTodo();
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(+d);
+    d.setHours(0,0,0);
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setDate(d.getDate() + 4 - (d.getDay()||7));
+    // Get first day of year
+    var yearStart = new Date(d.getFullYear(),0,1);
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7)
+    // Return array of year and week number
+    return [d.getFullYear(), weekNo];
 }
